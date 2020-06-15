2e871bdfb4b4be422b23da7a74377bc45651ec52
Yi Zhang <zhangyiss@icloud.com>
2020-06-15 15:00:20 +0800

    update on liblcg.rb to v1.5.1

--- Formula/liblcg.rb
+++ Formula/liblcg.rb
@@ -1,8 +1,11 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Liblcg < Formula
-  desc "A simple C++ library of linear conjugate gradient method."
+  desc "A simple C++ library of linear conjugate gradient method"
   homepage "https://yizhangcug.github.io/liblcg/"
   url "https://github.com/YiZhangCUG/liblcg/archive/v1.5.1.tar.gz"
-  sha256 "20edb4f14594dbc83c1dc61e22247f772d1ef01c37991de2fa8365ba0106cd70"
+  sha256 "3c4f31dea68c3a367089d635d29f1096c92506780b4c6d93435d17ccc63543c5"
 
   depends_on "cmake" => :build
 
@@ -12,5 +15,18 @@ class Liblcg < Formula
       system "make"
       system "make", "install" # if this fails, try separate make/make install steps
     end
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test liblcg`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
