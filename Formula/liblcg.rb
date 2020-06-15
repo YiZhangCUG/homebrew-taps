2c1c9d1d242af2f39b45ff73e6d39971b314d3e3
Yi Zhang <zhangyiss@icloud.com>
2020-06-15 08:07:47 +0800

    update on formula liblcg

--- Formula/liblcg.rb
+++ Formula/liblcg.rb
@@ -2,7 +2,7 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Liblcg < Formula
-  desc "A simple C++ library of linear conjugate gradient method"
+  desc "A simple C++ library of linear conjugate gradient method."
   homepage "https://yizhangcug.github.io/liblcg/"
   url "https://github.com/YiZhangCUG/liblcg/archive/v1.4.1.tar.gz"
   sha256 "d2ae281fdb85775e193a987ffb7a43155bc55c8a01feefe69ecc609d605f80cb"
@@ -10,12 +10,6 @@ class Liblcg < Formula
   depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
