3a14dabd62138217f5a611af797cb864ebaca5af
Yi Zhang <zhangyiss@icloud.com>
2020-06-15 17:49:54 +0800

    add formula for liblbfgs

---
+++ Formula/liblbfgs.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Liblbfgs < Formula
+  desc "libLBFGS: a library of Limited-memory Broyden-Fletcher-Goldfarb-Shanno (L-BFGS)"
+  homepage "http://www.chokkan.org/software/liblbfgs/"
+  url "https://github.com/YiZhangCUG/liblbfgs/archive/v1.1.0.tar.gz"
+  sha256 "cf65e6bd80b7976634dea99b95785df8045c30f1d040c28ccbb2ab367c4d648a"
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "install" # if this fails, try separate make/make install steps
+    end
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test liblbfgs`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
